36e886787c61085b32acc99c2088d76ae12c2419
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 10:41:56 +0900

    feat: setup base template and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-	return <></>;
+	// const url = `https://api.openweathermap.org/data/2.5/weather?q=nagoya&appid=${process.env.OPEN_WEATHER_API_KEY}`;
+
+	return (
+		<div className='app'>
+			<div className='container'>
+				<div className='top'>
+					<div className='location'>Nagoya</div>
+					<div className='temp'>
+						<h1>32°C</h1>
+					</div>
+					<div className='description'>
+						<p>Clouds</p>
+					</div>
+				</div>
+				<div className='bottom'>
+					<div className='feels'>
+						<p>36°C</p>
+					</div>
+					<div className='humidity'>
+						<p>20%</p>
+					</div>
+					<div className='wind'>12 MPH</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;

06074414c9b2521c018df424b6cb91cbd833f444
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 10:57:56 +0900

    feat: added base input handler logic and styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-	// const url = `https://api.openweathermap.org/data/2.5/weather?q=nagoya&appid=${process.env.OPEN_WEATHER_API_KEY}`;
+	const [data, setData] = useState({});
+	const [location, setLocation] = useState('');
+
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPEN_WEATHER_API_KEY}`;
+
+	const handleSearch = (event) => {
+		if (event.key === 'Enter') {
+			axios.get(url).then((res) => {
+				setData(res.data);
+			});
+
+			setLocation('');
+		}
+	};
 
 	return (
 		<div className='app'>
+			<div className='search'>
+				<input
+					type='text'
+					value={location}
+					placeholder='Enter Location...'
+					onChange={(event) => setLocation(event.target.value)}
+					onKeyPress={handleSearch}
+				/>
+			</div>
 			<div className='container'>
 				<div className='top'>
 					<div className='location'>Nagoya</div>
@@ -18,12 +40,17 @@ function App() {
 				</div>
 				<div className='bottom'>
 					<div className='feels'>
-						<p>36°C</p>
+						<p className='bold'>36°C</p>
+						<p>Feels Like</p>
 					</div>
 					<div className='humidity'>
-						<p>20%</p>
+						<p className='bold'>20%</p>
+						<p>Humidity</p>
 					</div>
-					<div className='wind'>12 MPH</div>
+					<div className='wind'>
+						<p className='bold'>12MPH</p>
+						<p>Wind Speed</p>
+					</div>
 				</div>
 			</div>
 		</div>

cb80b3b900367cf5b76f0b0577a7dd0f1aa1cd1c
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 11:15:56 +0900

    feat: updated rendering logic

--- src/App.js
+++ src/App.js
@@ -32,28 +32,32 @@ function App() {
 				<div className='top'>
 					<div className='location'>{data.name}</div>
 					<div className='temp'>
-						{data.main ? <h1>{data.main.temp}°C</h1> : null}
+						{data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
 					</div>
 					<div className='description'>
 						{data.weather ? <p>{data.weather[0].main}</p> : null}
 					</div>
 				</div>
-				<div className='bottom'>
-					<div className='feels'>
-						{data.main ? (
-							<p className='bold'>{data.main.feels_like}°C</p>
-						) : null}
-						<p>Feels Like</p>
+				{data.name != undefined && (
+					<div className='bottom'>
+						<div className='feels'>
+							{data.main ? (
+								<p className='bold'>{data.main.feels_like.toFixed()}°C</p>
+							) : null}
+							<p>Feels Like</p>
+						</div>
+						<div className='humidity'>
+							{data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+							<p>Humidity</p>
+						</div>
+						<div className='wind'>
+							{data.wind ? (
+								<p className='bold'>{data.wind.speed.toFixed()} m/s</p>
+							) : null}
+							<p>Wind Speed</p>
+						</div>
 					</div>
-					<div className='humidity'>
-						{data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-						<p>Humidity</p>
-					</div>
-					<div className='wind'>
-						{data.wind ? <p className='bold'>{data.wind.speed}m/s</p> : null}
-						<p>Wind Speed</p>
-					</div>
-				</div>
+				)}
 			</div>
 		</div>
 	);
